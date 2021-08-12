31f28d0a2a408ab13a9ae9f3cd43c74c98524543
Alexander Tejeda <AlexanderTejedaBarahona10@gmail.com>
2021-08-12 11:23:50 -0600

    Funcion de suma y resta

--- Proyecto/lexicalAnalyzer.py
+++ Proyecto/lexicalAnalyzer.py
@@ -19,14 +19,14 @@ class LexicalAnalyzer:
     def createSTable(self):
         sourceCode1 = sub(r'\n+', '$', self.sourceCode)
         while len(sourceCode1) != 0:
-            if match(r'(int|str)\s*', sourceCode1) != None: 
+            if match(r'(int|str|show)\s*', sourceCode1) != None: 
                 sourceCode1 = self.assignEntry2SymbolTable(match(r'(int|str)\s*', sourceCode1), sourceCode1, 100)
             elif match(r'[a-zA-Z]\w{0,14}\s*', sourceCode1) != None:
                 sourceCode1 = self.assignEntry2SymbolTable(match(r'[a-zA-Z]\w{0,14}\s*', sourceCode1), sourceCode1, 200)
             elif match(r'(\(|\)|=|>|\+|-|/|,|\$)\s*', sourceCode1) != None:
                 sourceCode1 = self.assignEntry2SymbolTable(match(r'(\(|\)|=|>|\+|-|/|,|\$)\s*', sourceCode1), sourceCode1, 300)
-            elif match(r"('[^']*'|\d)\s*", sourceCode1) != None: 
-                sourceCode1 = self.assignEntry2SymbolTable(match(r"('[^']*'|\d)\s*", sourceCode1), sourceCode1, 400)        
+            elif match(r"('[^']*'|\d+)\s*", sourceCode1) != None: 
+                sourceCode1 = self.assignEntry2SymbolTable(match(r"('[^']*'|\d+)\s*", sourceCode1), sourceCode1, 400)        
         del(sourceCode1)
 
     def assignEntry2SymbolTable(self, lexeme, sourceCode, code):
--- Proyecto/syntancticAnalyzer.py
+++ Proyecto/syntancticAnalyzer.py
@@ -1,5 +1,6 @@
-from os import truncate
+# Expresion regular para funcion contact: concat\(((\w+),)+\w\)
 from re import search
+from operator import add, sub
 class SyntacticAnalyzer:
     
     def __init__(self, symbolTable): 
@@ -42,28 +43,37 @@ class SyntacticAnalyzer:
         return self.symbolTable
     
     def doIt(self):
-        line = 0
-        for i in range(0, len(self.symbolTable)):
+        i = 0
+        while i < len(self.symbolTable): 
             if self.symbolTable[i][1] == 100:
-                # self.changeValue(i, True)
-                line, i = self.reserveMemory(i, line)                
-        self.output += "Codigo finalizado"
-    
-    def reserveMemory(self, i, line):
+                i = self.reserveMemory(i)
+            else: 
+                if(self.changeValue(i, False)):
+                    if(self.changeOperation(i)):
+                        pass
+                    else: 
+                        self.output += "Error:4\nError de sintaxis"
+                        break
+                else: 
+                    self.output += "Error:3\nReferencia a memoria no encontrada\n"    
+                    break
+                i = self.finalInstruction[0] + 1
+                del(self.finalInstruction[0])
+        self.output += "El Codigo finalizo"
+        
+    def reserveMemory(self, i):
         if(search(r"^(\w)+=('[^']*'|\d+|(\w)+)", "".join(self.lexeme[i+1:self.finalInstruction[0]]))) != None:
             if(self.changeValue(i, True)):
                 if(self.symbolTable[i][0]) == "str":        
                     if(search(r"'[^']*'", self.obtainLexeme(i, 400)) != None):
                         self.saveValue(self.obtainLexeme(i, 200), self.obtainLexeme(i, 400), "str")
-                        line = line + 1
                     else: 
                         i = len(self.symbolTable)
-                        self.output += "Error:1\nError de tipado\nLine:"
+                        self.output += "Error:1\nError de tipado:"
                 # ¿En esta parte se tiene que aclarar el tipo de la variable?
                 else:
                     if(search(r"\d+", self.obtainLexeme(i, 400)) != None):
                         self.saveValue(self.obtainLexeme(i, 200), self.obtainLexeme(i, 400), "int")
-                        line = line + 1
                     else: 
                         i = len(self.symbolTable)
                         self.output += "Error:1\nError de tipado\n:"
@@ -73,9 +83,9 @@ class SyntacticAnalyzer:
         else:
             i = len(self.symbolTable)
             self.output += "Error:2\nSintaxis erronea\n"        
-        
+        newLine = self.finalInstruction[0] + 1
         del(self.finalInstruction[0])
-        return line, i
+        return newLine
 
     def obtainLexeme(self, i, code):
         return self.lexeme[i + 1: self.finalInstruction[0]][self.code[i + 1 : self.finalInstruction[0]].index(code)]
@@ -100,17 +110,61 @@ class SyntacticAnalyzer:
             start = self.code[i : self.finalInstruction[0]].index(300)
         for j in range(0, len(self.code[i : self.finalInstruction[0]])): 
             if self.code[i : self.finalInstruction[0]][j] == 200: 
-                if j > start:
+                if j >= start:
                     if (self.lexeme[i : self.finalInstruction[0]][j] in self.memoryStr) == True:
-                        self.code[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)] = 400
-                        self.symbolTable[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)][1] = 400
-                        self.symbolTable[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)][0] = self.memoryStr[self.lexeme[i : self.finalInstruction[0]][j]]
-                        self.lexeme[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)] = self.memoryStr[self.lexeme[i : self.finalInstruction[0]][j]] 
+                        self.setSymbolTable(i, j, 400)
                     elif (self.lexeme[i : self.finalInstruction[0]][j] in self.memoryInt) == True:
-                        self.code[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)] = 400
-                        self.symbolTable[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)][1] = 400
-                        self.symbolTable[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)][0] = self.memoryInt[self.lexeme[i : self.finalInstruction[0]][j]]
-                        self.lexeme[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)] = self.memoryInt[self.lexeme[i : self.finalInstruction[0]][j]] 
+                        self.setSymbolTable(i, j, 400)
                     else: 
                         return False
         return True
+
+    def findFunction(self):
+        pass
+    
+    def changeOperation(self, j):
+        if '+' in self.lexeme[j : self.finalInstruction[0]] or '-' in self.lexeme[j : self.finalInstruction[0]]:
+            if search(r'\d+(\+|-)\d+', "".join(self.lexeme[j : self.finalInstruction[0]])) != None:
+                coincidence = search(r'\d+(\+|-)', "".join(self.lexeme[j : self.finalInstruction[0]])).group()
+                coincidence1 = search(r'(\+|-)\d+', "".join(self.lexeme[j : self.finalInstruction[0]])).group()
+                coincidence = coincidence[0:len(coincidence) - 1]
+                operator = coincidence1[0]
+                coincidence1 = coincidence1[1:]
+                if operator == "+":
+                    value = self.lexeme[j : self.finalInstruction[0]].index("+")
+                    self.symbolTable[j + value - 1] = [str(add(int(coincidence), int(coincidence1))), 400]
+                    self.lexeme[j + value - 1] = str(add(int(coincidence), int(coincidence1)))
+                    self.code[j + value - 1] = 400
+                    del(self.symbolTable[j + value])   
+                    del(self.symbolTable[j + value])
+                    del(self.code[j + value])   
+                    del(self.code[j + value])   
+                    del(self.lexeme[j + value])   
+                    del(self.lexeme[j + value])
+                else:
+                    value = self.lexeme[j : self.finalInstruction[0]].index("-")
+                    self.symbolTable[j + value - 1] = [str(add(int(coincidence), -int(coincidence1))), 400]
+                    self.lexeme[j + value - 1] = str(add(int(coincidence), -int(coincidence1)))
+                    self.code[j + value - 1] = 400
+                    del(self.symbolTable[j + value])   
+                    del(self.symbolTable[j + value])
+                    del(self.code[j + value])   
+                    del(self.code[j + value])   
+                    del(self.lexeme[j + value])   
+                    del(self.lexeme[j + value])                    
+                return True
+            else: 
+                return False
+
+    def setSymbolTable(self, i, j, code):
+        try: 
+            self.code[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)] = code
+            self.symbolTable[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)][1] = code
+            self.symbolTable[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)][0] = self.memoryStr[self.lexeme[i : self.finalInstruction[0]][j]]
+            self.lexeme[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)] = self.memoryStr[self.lexeme[i : self.finalInstruction[0]][j]] 
+        except: 
+            self.code[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)] = code
+            self.symbolTable[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)][1] = code
+            self.symbolTable[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)][0] = self.memoryInt[self.lexeme[i : self.finalInstruction[0]][j]]
+            self.lexeme[(self.finalInstruction[0] - len(self.code[i : self.finalInstruction[0]]) + j)] = self.memoryInt[self.lexeme[i : self.finalInstruction[0]][j]] 
+        
